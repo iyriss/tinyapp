9f4fc1dc4a25f96af83d0e61071fcfea1328122b
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-01 07:08:55 +0000

    generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let randomUrl = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+  for (let i = 0; i < length ; i++) {
+    randomUrl += chars[Math.round(Math.random() * (chars.length))]
+  } return randomUrl;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,19 +29,29 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+//This app.get("/urls/new", should be above app.get("/urls/:id", ...)
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//urls_show:
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL  };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/u/:shortURL2", (request, res) => {
+  // console.log(request)
+  let longURL = urlDatabase[request.params.shortURL2];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
