5a83d8f9ad4365b4bdfedebfac2901777e88aa93
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-04 14:50:33 +0000

    eslint and refactoring code

--- helpers.js
+++ helpers.js
@@ -1,8 +1,27 @@
 const getUserByEmail = function(email, database) {
-  for(const userId in database)
-  if (database[userId].email === email) {
-    return userId;
-  }
+  for (const userId in database)
+    if (database[userId].email === email) {
+      return userId;
+    }
 };
 
-module.exports = { getUserByEmail } ;
+const generateRandomString = () => {
+  let randomUrl = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    randomUrl += chars[Math.round(Math.random() * (chars.length))];
+  } return randomUrl;
+};
+
+const urlsForUser = (id, urlDatabase) => {
+  const urls = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url]["userID"] === id) {
+      urls[url] = urlDatabase[url];
+    }
+  }
+  return urls;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
